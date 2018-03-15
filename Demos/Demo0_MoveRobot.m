1aa5d385c8b3a990399f7f885b6e096465240903
Nick Van Oosterwyck <NickVanOosterwyck@users.noreply.github.com>
2018-03-15 10:27:51 +0100

    Add Demo 0

--- Demos/Demo0_MoveRobot.m
+++ Demos/Demo0_MoveRobot.m
@@ -9,14 +9,7 @@ rob=ur10core('vrep');    %-- choose UR10
 %rob=ur10core('real');
 rob.connect();
 
-cam=kinectcore('vrep');  %-- choose kinect
-%cam=kinectcore('real');
-cam.connect();
-
 %% Set up
-%-- move camera
-%cam.moveToCameraLocation([2.03 2.03 1.08 90 -45 0]); % north-east
-
 %-- set positions
 Home = rob.homeJointTargetPositions;
 PickUp = [45 -110 -80 -170 -135 0];
@@ -26,13 +19,6 @@ PlaceApp = [-25 -113.2953  -44.7716 -201.9331 -25 0];
 
 %-- create path
 Path =[Home;PickUpApp;PickUp;PickUpApp;PlaceApp;Place;PlaceApp;Home];
-
-%-- set safety distances
-rStop = 3/2;
-
-%% Check pointclouds
-cam.getPointCloudCalibration();
-cam.getPointCloudComparison();
 
 %% Go home
 rob.goHome(0.1);
@@ -54,24 +40,13 @@ for it = 1:iterations
             %tic
             [dist,~] = cam.getClosestPoint();
             %toc
-            if dist < rStop
-                if state ~=0 && (state ==1 || state ==2)
-                rob.stopRobot();
-                state = 0; disp('Robot is stopped')
-                end
-            else
-                if  state ~=2 && (state ==0 || state ==1)
+            
+            if  state ~=2 && state ==1
                 rob.moveToJointTargetPositions(Path(i,:),MaxSpeedFactor);
                 state = 2;
-                end
             end
+            
         end
     end
 end
-
-%% States
-% 0	Stop
-% 1	Next Target
-% 2	Move normal
-% 3	Move slow
 
